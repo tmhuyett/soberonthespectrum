7ee203f9e9b7c078a6b53d846a4d7b3de3427c73
tmhuyett <toddhuyett@gmail.com>
2025-10-22 07:28:46 -0400

    changed message when user isn't logged in

--- app/Main.tsx
+++ app/Main.tsx
@@ -19,7 +19,7 @@ export default function Home({ posts }) {
           </p>
         </div>
         <ul className="divide-y divide-gray-200 dark:divide-gray-700">
-          {!posts.length && 'No posts found.'}
+          {!posts.length && 'Please log in to participate.'}
           {posts.slice(0, MAX_DISPLAY).map((post) => {
             const { slug, date, title, summary, tags } = post
             return (
